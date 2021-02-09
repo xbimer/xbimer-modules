f679eb78eb82c45f5d61be1dd178875197934dac
beanjs <445212619@qq.com>
2021-02-09 15:04:21 +0800

    1.add Graphix module

---
+++ Graphix/src/Graphix.cpp
@@ -0,0 +1,18 @@
+#include "stdafx.h"
+
+#if !defined(ARCHICAD_NONE)
+#include "cast-interface.h"
+#endif
+
+#include "Python.h"
+#include "pybind11.h"
+
+// https://pybind11.readthedocs.io/en/stable/
+using namespace pybind11;
+
+#define PROJECT_VERSION "1.0.0"
+
+
+PYBIND11_MODULE(Graphix, m) {
+	m.add_object("version", str(PROJECT_VERSION));//Please do not delete the module version number
+}
---
+++ Graphix/src/stdafx.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "targetver.h"
+
+#define WIN32_LEAN_AND_MEAN
+
+#if !defined(ARCHICAD_NONE)
+#include "Win32Interface.hpp"
+#pragma warning (disable: 4068)
+#endif
+
+#include <windows.h>
